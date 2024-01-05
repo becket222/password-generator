47d478196b5d5cc259193f706a6bd536fa31a290
becket <rebeca.aruxandei@gmail.com>
2024-01-05 22:43:01 +0000

    Worked on 01.Function to prompt user for password options & added notes

--- starter/script.js
+++ starter/script.js
@@ -88,25 +88,55 @@ var upperCasedCharacters = [
   'Z'
 ];
 
-// Function to prompt user for password options
+// 01. Function to prompt user for password options
 function getPasswordOptions() {
-
+  var characterLength = parseInt(prompt("How many characters would you like your password to have? (8-128)"));
+  if (characterLength < 8 || characterLength > 128) {
+    alert("Sorry, incorrect imput! Value must be a number between 8 and 128.");
+    return;
+  }
+   // add -> 'if' statement for if they click 'ok' withouth putting anything in
+   // or putting whitespace characters 
+  if ( characterLength.trim().length === 0) {
+    alert("Field empty! Please insert numerical value.");
+  } else if (characterLength === null) {
+    alert("Field null! Please insert numerical value.");
+  }
+  var characterTypeUpper = confirm("Would you like your password to include uppercase characters?");
+  var characterTypeLower = confirm("Would you like your password to include lowercase characters?");
+  var characterTypeNumber = confirm("Would you like your password to include numbers?");
+  var characterTypeSpecial = confirm("would you like your password to include special characters? ($@%&*, etc)");
+  return {
+    length: characterLength,
+    uppercase: characterTypeUpper,
+    lowercase: characterTypeLower,
+    number: characterTypeNumber,
+    special: characterTypeSpecial,
+  } 
 }
 
-// Function for getting a random element from an array
+// 02. Function for getting a random element from an array
 function getRandom(arr) {
 
 }
+   // get random index from array
+   // use index to get random elemnt and return it
+   // combineine arrays?????
 
-// Function to generate password with user input
+// 03. Function to generate password with user input
 function generatePassword() {
+  var userImput = getPasswordOptions()
 
+  if ()
+
+   // call function (can be done here or in writePassword function)
+getPasswordOptions()
 }
 
-// Get references to the #generate element
+// 04. Get references to the #generate element
 var generateBtn = document.querySelector('#generate');
 
-// Write password to the #password input
+// 05. Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector('#password');
@@ -114,5 +144,40 @@ function writePassword() {
   passwordText.value = password;
 }
 
-// Add event listener to generate button
+// 06. Add event listener to generate button
 generateBtn.addEventListener('click', writePassword);
+
+
+
+
+// NOTES & Internal Dilemmas:
+
+// characterLength promt by default returns a string but we need a number
+// so use 'parseInt()' -> takes a string and returns a number ()
+// put whole promt inside ()
+
+
+// DIFFERENCE betweent empty, null, & whitespace (strings):
+// https://www.freecodecamp.org/news/check-if-string-is-empty-or-null-javascript/
+// empty = if string length is 0 
+// (an empty string is a valid string object so all string operations are available on this item)
+// check !! -> if (typeof characterLength === "string" && characterLength.length === 0)
+
+// null = the absence of any value 
+// implies that it doesn’t refer to any object or value in the memory. By default, Java initializes reference variables 
+// with null values and primitives with default values -> cannot assign null to primitives.
+
+// whitespace = whitespace characters that make it appear non-empty even when it is.
+// '.trim()' to remove any leading or trailing whitespace characters before checking for emptiness
+
+
+// 'return' statemnet CONFUSION passing along user imput from 01 -> 03:
+// 1. I don't need to save the user imput return object in a variable (e.g userImput) 
+// 2. If I did I wouldn't be able to use it in 03 anyaway (bc that variable is local to that function only)
+// 3. So, you just create the variable 'userImput' in 03 and call the 01 function inside of that 
+// SO -> var userImput = getPasswordOptions() 
+
+
+// Where to CALL for password to do its thing:
+// might not be the best option to call the function in writePassword (05) 
+// but rather to call it in generatePassword (03) function instead 
